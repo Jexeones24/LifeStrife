5e473c1f04a705c5fe3e5d99ce2258a71787d635
Jexeones24 <jexeones@gmail.com>
2017-09-12 12:08:46 -0400

    styled decision form

--- src/containers/DecisionForm.js
+++ src/containers/DecisionForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Comment, Form, Header } from 'semantic-ui-react'
+import { Button, Comment, Form, Header, Grid, Segment, Image } from 'semantic-ui-react'
 
 export default class DecisionForm extends Component {
   constructor(){
@@ -25,32 +25,47 @@ export default class DecisionForm extends Component {
       console.log(this.props)
       return (
         <div className="decision-form">
-          <Comment.Group>
-            <Header as='h1' dividing>MAKE A DECISION</Header>
-            <Comment>
-              <Comment.Avatar src='https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?dpr=2&auto=format&fit=crop&w=1199&h=1493&q=80&cs=tinysrgb&crop=' />
-              <Comment.Content>
-                <Comment.Author as='a'><h3>{this.props.currentUser}</h3></Comment.Author>
-                <Comment.Metadata>
-                  <div>5 days ago</div>
-                </Comment.Metadata>
-                <Comment.Text>
+          <Grid columns='equal'>
+            <Grid.Row stretched>
+              <Grid.Column>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Segment>
+                  <Comment.Group>
+                    <Header as='h1' dividing>MAKE A DECISION</Header>
+                    <Comment>
+                      <Comment.Content>
+                        <Comment.Author as='a'><h3>{this.props.currentUser}</h3></Comment.Author>
+                        <Comment.Text>
 
-                 <h3>Give a brief description of what's weighing on your mind...</h3>
+                         <h3>Give a brief description of what's weighing on your mind...</h3>
 
-                </Comment.Text>
-                <Comment.Actions>
-                  <Comment.Action></Comment.Action>
-                </Comment.Actions>
-              </Comment.Content>
-            </Comment>
+                        </Comment.Text>
+                        <Comment.Actions>
+                          <Comment.Action></Comment.Action>
+                        </Comment.Actions>
+                      </Comment.Content>
+                    </Comment>
 
-          <Form onSubmit={this.handleSubmit} reply>
-            <Form.TextArea type="text" placeholder="Decision" name="content" value={this.state.content} onChange={this.handleChange} required/>
-            <Button content='Submit Decision' labelPosition='left' icon='edit' primary />
-          </Form>
-        </Comment.Group>
-
+                  <Form onSubmit={this.handleSubmit} reply>
+                    <Form.TextArea type="text" placeholder="Decision" name="content" value={this.state.content} onChange={this.handleChange} required/>
+                    <Button content='Submit Decision' labelPosition='left' icon='edit' primary />
+                  </Form>
+                </Comment.Group>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+            </Grid.Column>
+            <Grid.Column width={6}>
+            </Grid.Column>
+            <Grid.Column>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
